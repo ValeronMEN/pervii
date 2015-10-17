d6a181d767cfab52b86eafed118f746a1ad720ff
Valeriy <valeroon.men@gmail.com>
2015-10-18 02:06:01 +0300

    L2D

---
+++ courses/prog_base/tasks/long2double/repr.c
@@ -0,0 +1,45 @@
+#include <math.h>
+
+double long2double (long long bitcode)
+{
+    int b[64];
+    for (int i=63; i>=0; bitcode>>=1, i--)
+        b[i]=bitcode & 1;
+
+    int sign=b[0];
+
+    int exponent=0;
+    for (int i=1; i<=bitcode; i++)
+        if (exponent = exponent + b[i])
+        return (int)pow(2, bitcode-i);
+          else
+        return 0;
+
+    double mantissa=0;
+    for (int i=12; i<=63; i++)
+        if (mantissa = mantissa + b[i])
+        return pow(2, bitcode - i);
+          else
+        return 0;
+
+    if (exponent>0 && exponent<2047)
+        return pow(-1, sign) * pow(2, exponent-1023) * (mantissa+1);
+    if (exponent==0 && mantissa!=0)
+        return pow(-1, sign) * pow(2, -1022)*mantissa;
+
+    if (sign==1 && exponent==0 && mantissa==0)
+        return -0.;
+    if (sign==0 && exponent==0 && mantissa==0)
+        return 0.;
+
+    if (sign==1 && exponent==2047 && mantissa==0)
+        return -INFINITY;
+    if (sign==0 && exponent==2047 && mantissa==0)
+        return INFINITY;
+
+    if (exponent==2047 && mantissa!=0)
+        return NAN;
+
+
+
+}
