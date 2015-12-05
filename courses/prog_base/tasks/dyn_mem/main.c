329dd71f820b8224b834bdb0a83f6857b69e8451
Valeriy <valeroon.men@gmail.com>
2015-12-05 14:55:09 +0200

    Dynamic Memory Management

---
+++ courses/prog_base/tasks/dyn_mem/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    char * tmp;
+    int * minimal;
+    int * i;
+
+    tmp = malloc(100*sizeof(char));
+    minimal = malloc(sizeof(int));
+    i = malloc(sizeof(int));
+
+    gets(tmp);
+    *minimal = *tmp;
+
+    for((*i)=0; (*i)<strlen(tmp); (*i)++){
+        if (*(tmp+*i)<(*minimal)){
+            *minimal = *(tmp+*i);
+        }
+    }
+
+    printf("Character with minimum ASCII code has code: %i,\nCharacter is %c\n", *minimal, *minimal);
+
+    return 0;
+}
