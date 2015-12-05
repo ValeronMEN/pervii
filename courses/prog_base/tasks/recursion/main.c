0f970129f6f3537acbd8f4716309b9215b39fc5d
Valeriy <valeroon.men@gmail.com>
2015-12-06 01:48:27 +0200

    Recursive Functions

---
+++ courses/prog_base/tasks/recursion/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void){
+    char stroka[100];
+    int * pointer;
+    int k=0;
+
+    pointer = &k;
+
+    gets(stroka);
+    printf("An amount of elements in string: %i\n", strlen(stroka));
+    return function1(stroka, pointer, strlen(stroka));
+}
+
+int function1(char * stroka, int * pointer, int size){
+    if (*pointer == 5) return 1;
+
+    if (size==0) return 0;
+
+    printf("String is: ");
+    puts(stroka);
+
+    if(isdigit(*stroka)){
+        (*pointer)++;
+        printf("Am amount of numbers: %i\n", *pointer);
+    }
+
+    function1(stroka+1, pointer, size-1);
+
+    if (*pointer == 5) return 1;
+
+    return 0;
+}
