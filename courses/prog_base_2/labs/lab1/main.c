6925dd99c486ca7f630c3c852174d475172fada0
Valeriy <valeroon.men@gmail.com>
2016-03-18 17:30:41 +0200

    Lab 1 fix 2

--- courses/prog_base_2/labs/lab1/main.c
+++ courses/prog_base_2/labs/lab1/main.c
@@ -8,43 +8,58 @@
 #include "heap.h"
 #include "memory.h"
 
-static void newAndNewManuallyHeaps_void_zeroCount(void **state){
+static void new_void_zeroCount(void **state){
     heap_t * heap = heap_new(45);
     assert_int_equal(heap_getSize(heap), 100);
     assert_int_equal(heap_getstatus(heap), HEAP_OK);
     heap_free(heap);
-    heap = heap_new_manually(4567, 1);
+}
+
+static void newManually_void_zeroCount(void **state){
+    heap_t * heap = heap_new_manually(4567, 1);
     assert_int_equal(heap_getSize(heap), 1);
     assert_int_equal(heap_getstatus(heap), HEAP_OK);
     heap_free(heap);
 }
 
-static void newMemory_overflowAndNormalSizesOfMemories_oneEmptyMemoryCount(void **state){
+static void newMemory_normalSizeOfMemory_onePassed(void **state){
+    heap_t * heap = heap_new(67);
+    memory_t * memory = heap_newMemory(heap, 40);
+    assert_int_equal(heap_getSize(heap), 60);
+    assert_int_equal(heap_getstatus(heap), HEAP_OK);
+    assert_int_equal(memory_getstatus(memory), MEMORY_EMPTY);
+    heap_free(heap);
+}
+
+static void newMemory_heapOverflow_nothingPassed(void **state){
     heap_t * heap = heap_new(67);
     memory_t * memory = heap_newMemory(heap, 300);
     assert_int_equal(heap_getSize(heap), 100);
     assert_int_equal(heap_getstatus(heap), HEAP_OVERFLOW);
-    assert_int_equal(memory_getstatus(memory), MEMORY_EMPTY);
-    heap_newMemory(heap, 40);
-    assert_int_equal(heap_getSize(heap), 60);
-    assert_int_equal(heap_getstatus(heap), HEAP_OK);
     assert_int_equal(memory_getstatus(memory), MEMORY_EMPTY);
     heap_free(heap);
 }
 
 static void freeMemory_oneMemory_zeroCount(void **state){
     heap_t * heap = heap_new(23452);
-    heap_t * heap2 = heap_new(1122);
     memory_t * memory = heap_newMemory(heap, 40);
-    heap_freeMemory(memory, heap2);
-    assert_int_equal(heap_getSize(heap), 60);
-    assert_int_equal(memory_getstatus(memory), MEMORY_BADHEAP);
     heap_freeMemory(memory, heap);
     assert_int_equal(heap_getSize(heap), 100);
     heap_free(heap);
 }
 
-static void memoryEnter_EmptyNormalOverflowAndBaddataMemories_oneCount(void **state){
+static void freeMemory_oneMemoryBadHeap_nothingPassed(void **state){
+    heap_t * heap = heap_new(23452);
+    heap_t * heap2 = heap_new(1122);
+    memory_t * memory = heap_newMemory(heap, 40);
+    heap_freeMemory(memory, heap2);
+    assert_int_equal(heap_getSize(heap), 60);
+    assert_int_equal(memory_getstatus(memory), MEMORY_BADHEAP);
+    heap_free(heap);
+    heap_free(heap2);
+}
+
+static void memoryEnter_EmptyNormalOverflowAndBaddataMemories_onePassed(void **state){
     heap_t * heap = heap_new(45);
     memory_t * memory = heap_newMemory(heap, 40);
     memory_enter(memory, "11 00 1101");
@@ -52,7 +67,7 @@ static void memoryEnter_EmptyNormalOverflowAndBaddataMemories_oneCount(void **st
     memory_enter(memory, "");
     assert_int_equal(memory_getstatus(memory), MEMORY_EMPTY);
     heap_free(heap);
-    heap = heap_new(45);
+    heap = heap_new(46);
     memory = heap_newMemory(heap, 10);
     memory_enter(memory, "hdjfhdh");
     assert_int_equal(memory_getstatus(memory), MEMORY_BADDATA);
@@ -61,11 +76,17 @@ static void memoryEnter_EmptyNormalOverflowAndBaddataMemories_oneCount(void **st
     heap_free(heap);
 }
 
-static void memoryReadAndGetSize_oneMemory_textAndSizeFromStart(void **state){
+static void memoryRead_oneMemory_oneRead(void **state){
     heap_t * heap = heap_new(45);
     memory_t * memory = heap_newMemory(heap, 40);
     memory_enter(memory, "11");
     assert_int_equal(strcmp(memory_read(memory), "11"), 0);
+    heap_free(heap);
+}
+
+static void memoryGetSize_oneMemory_sizePassed(void **state){
+    heap_t * heap = heap_new(45);
+    memory_t * memory = heap_newMemory(heap, 40);
     assert_int_equal(memory_getSize(memory), 40);
     heap_free(heap);
 }
@@ -73,11 +94,15 @@ static void memoryReadAndGetSize_oneMemory_textAndSizeFromStart(void **state){
 int main(){
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test(newAndNewManuallyHeaps_void_zeroCount),
-        cmocka_unit_test(newMemory_overflowAndNormalSizesOfMemories_oneEmptyMemoryCount),
+        cmocka_unit_test(new_void_zeroCount),
+        cmocka_unit_test(newManually_void_zeroCount),
+        cmocka_unit_test(newMemory_normalSizeOfMemory_onePassed),
+        cmocka_unit_test(newMemory_heapOverflow_nothingPassed),
         cmocka_unit_test(freeMemory_oneMemory_zeroCount),
-        cmocka_unit_test(memoryEnter_EmptyNormalOverflowAndBaddataMemories_oneCount),
-        cmocka_unit_test(memoryReadAndGetSize_oneMemory_textAndSizeFromStart),
+        cmocka_unit_test(freeMemory_oneMemoryBadHeap_nothingPassed),
+        cmocka_unit_test(memoryEnter_EmptyNormalOverflowAndBaddataMemories_onePassed),
+        cmocka_unit_test(memoryRead_oneMemory_oneRead),
+        cmocka_unit_test(memoryGetSize_oneMemory_sizePassed),
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
