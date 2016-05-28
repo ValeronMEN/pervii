5c820f14dd68a508545909fc9a43db62e5a0ef3d
Valeriy <valeroon.men@gmail.com>
2016-05-28 12:15:50 +0300

    Test 2 #3

--- courses/prog_base_2/tests/test_2/client/functions.h
+++
@@ -1,4 +0,0 @@
-char * function_request(SOCKET recvSocket, const char request [], char buffer [], int maxLen);
-char * function_secret(char * buffer_p);
-void function_list(char * list, char * buffer_p);
-void function_nameOfMax(char * list, int size, char * strmax);
